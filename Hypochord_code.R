f0a78cf5a6e706bb2df533d90b955b77a9be7dcd
Gayani Senevirathne <46111490+GayaniSenevirathne@users.noreply.github.com>
2020-04-17 01:36:41 -0500

    commit 3

--- Hypochord_code.R
+++ Hypochord_code.R
@@ -26,7 +26,46 @@ library("PoiClaClu")
 
 
 seqdata <- read.table("count_matrixH.tabular") 
-#removing first columns fro the uploaded file
+#removing first columns from the uploaded file
 countdata <- seqdata[,-(1)] 
 rownames(countdata) <- seqdata[,1]
 head(countdata)
+
+#defining the conditions for the hypochord
+condition <-factor(c (rep("methimazole", 1), rep("methimazole", 1), rep("control", 1), 
+                      rep("control", 1), rep("control", 1)))
+timepoints <-factor(c (rep ("t1", 2), rep("t3", 1), rep("t2", 1), rep("t1", 1)))
+sampleTable <-data.frame(timepoints = as.factor(timepoints), condition = as.factor(condition))
+rownames(sampleTable)=colnames(countdata)
+sampleTable
+
+
+#Differential expression analysis using DESeq2
+
+dds <- DESeqDataSetFromMatrix(countData = countdata, colData = sampleTable, design = ~timepoints+condition)
+dds
+
+#remove counts with less than 10
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+head(dds)
+dds$condition <- factor(dds$condition, levels = c("methimazole","control"))
+dds$timepoints <-factor(dds$timepoints, levels = c("t1","t2", "t3"))
+
+
+#running dds
+dds <- DESeq(dds)
+
+
+#Differential expression analysis
+#Specifying the coefficient or contrast we want to build a results table for...
+#Following command gives the contrast states you can test for the differential expression analysis
+resultsNames(dds) 
+res <- results(dds, contrast=c("condition","methimazole","control"))
+resultsNames(dds) #this gives the contrasts that can be done
+res
+summary(res)
+
+#PCA plot for the log transformed data
+rld <- rlog(dds, blind = FALSE)
+plotPCA(rld, intgroup = c("condition", "timepoints"))
